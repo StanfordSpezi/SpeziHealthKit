af2254fb94cbb2057fc1d87b2b03fc55b0d52766
Lukas Kollmer <hey@lukaskollmer.de>
2025-01-14 16:30:07 +0100

    fix typo

--- Sources/SpeziHealthKit/HealthKitDeliverySetting/HealthKitDeliverySetting.swift
+++ Sources/SpeziHealthKit/HealthKitDeliverySetting/HealthKitDeliverySetting.swift
@@ -10,13 +10,19 @@
 /// Determines the data delivery settings for any ``HealthKitDataSource`` used in the HealthKit module.
 public enum HealthKitDeliverySetting: Equatable {
     /// The HealthKit data is manually collected when the ``HealthKit/triggerDataSourceCollection()`` function is called.
-    case manual(safeAnchor: Bool = true)
+    case manual(saveAnchor: Bool = true)
     /// The HealthKit data is collected based on the `HealthKitDeliveryStartSetting` and constantly listens to updates while the application is running.
-    /// If `safeAnchor` is enabled the `HKQueryAnchor` is persisted across multiple application launches using the user defaults.
+    /// If `saveAnchor` is enabled the `HKQueryAnchor` is persisted across multiple application launches using the user defaults.
     case anchorQuery(HealthKitDeliveryStartSetting = .automatic, saveAnchor: Bool = true)
     /// The HealthKit data is collected based on the `HealthKitDeliveryStartSetting` and constantly listens to updates even if the application is not running.
-    /// If `safeAnchor` is enabled the `HKQueryAnchor` is persisted across multiple application launches using the user defaults.
+    /// If `saveAnchor` is enabled the `HKQueryAnchor` is persisted across multiple application launches using the user defaults.
     case background(HealthKitDeliveryStartSetting = .automatic, saveAnchor: Bool = true)
+    
+    /// :nodoc:
+    @available(*, unavailable, renamed: "manual(saveAnchor:)")
+    public static func manual(safeAnchor saveAnchor: Bool) -> Self {
+        .manual(saveAnchor: saveAnchor)
+    }
     
     
     var saveAnchor: Bool {
