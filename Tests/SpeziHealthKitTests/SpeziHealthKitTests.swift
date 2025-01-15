4bb52de032438f3d164a82a7cf88d9fb6ec0d4d1
Lukas Kollmer <hey@lukaskollmer.de>
2025-01-15 18:27:16 +0100

    RIP HealthKitTests (they were just checking the UserDefaults auth stuff anyway...)

--- Tests/SpeziHealthKitTests/SpeziHealthKitTests.swift
+++ Tests/SpeziHealthKitTests/SpeziHealthKitTests.swift
@@ -11,51 +11,9 @@ import HealthKit
 import XCTest
 import XCTSpezi
 
+
 final class SpeziHealthKitTests: XCTestCase {
-    static let collectedSamples: Set<HKSampleType> = [
-        HKQuantityType(.stepCount),
-        HKQuantityType(.distanceWalkingRunning)
-    ]
-    
-    @MainActor let healthKitModule = HealthKit {
-        CollectSamples(
-            collectedSamples,
-            deliverySetting: .anchorQuery(.automatic)
-        )
-    }
-    
     override func tearDown() {
         // Clean up UserDefaults
-        UserDefaults.standard.removeObject(forKey: UserDefaults.Keys.healthKitRequestedSampleTypes)
-    }
-    
-    /// No authorizations for HealthKit data are given in the ``UserDefaults``
-    @MainActor
-    func testSpeziHealthKitCollectionNotAuthorized1() {
-        XCTAssert(!healthKitModule.authorized)
-    }
-    
-    /// Not enough authorizations for HealthKit data given in the ``UserDefaults``
-    @MainActor
-    func testSpeziHealthKitCollectionNotAuthorized2() {
-        // Set up UserDefaults
-        UserDefaults.standard.set(
-            Array(Self.collectedSamples.map { $0.identifier }.dropLast()),  // Drop one of the required authorizations
-            forKey: UserDefaults.Keys.healthKitRequestedSampleTypes
-        )
-        
-        XCTAssert(!healthKitModule.authorized)
-    }
-    
-    /// Authorization for HealthKit data are given in the ``UserDefaults``
-    @MainActor
-    func testSpeziHealthKitCollectionAlreadyAuthorized() {
-        // Set up UserDefaults
-        UserDefaults.standard.set(
-            Self.collectedSamples.map { $0.identifier },
-            forKey: UserDefaults.Keys.healthKitRequestedSampleTypes
-        )
-        
-        XCTAssert(healthKitModule.authorized)
     }
 }
